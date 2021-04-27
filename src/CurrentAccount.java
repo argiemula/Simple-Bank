8a1dba26af353025692dcda1b92f9eb1d8a64c1b
Argie <sd.argie.mula.com>
2021-04-27 16:16:41 +0800

    Create CurrentAccount Class

---
+++ src/CurrentAccount.java
@@ -0,0 +1,21 @@
+public class CurrentAccount implements IBank{
+    @Override
+    public void withdraw(double amount) {
+        
+    }
+
+    @Override
+    public void deposit(double amount) {
+
+    }
+
+    @Override
+    public double balanceInquiry() {
+        return 0;
+    }
+
+    @Override
+    public void exit() {
+
+    }
+}
